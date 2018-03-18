f16d60644df2a942ecf58e784b0da02c2943cac1
vladosby <vladislavn.kievski@gmail.com>
2018-03-18 21:00:38 +0300

    Example of organizing libraries

---
+++ src/main/scala/com/scala/helloworld/TestJsonLib.scala
@@ -0,0 +1,9 @@
+package com.scala.helloworld
+
+import com.scala.helloworld.json._
+
+object TestJsonLib {
+  def main(args: Array[String]): Unit = {
+    printAsJson("ssssssss")
+  }
+}
---
+++ src/main/scala/com/scala/helloworld/package.scala
@@ -0,0 +1,8 @@
+package com.scala.helloworld
+
+package object json {
+
+  def printAsJson(s: String): Unit = {
+    println(s)
+  }
+}
