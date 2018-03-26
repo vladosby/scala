ab634e7918a18c668158ce332a3007231704b966
vladosby <vladislavn.kievski@gmail.com>
2018-03-26 12:40:27 +0300

    Add implicit examples

---
+++ src/main/scala/com/scala/helloworld/Implicits.scala
@@ -0,0 +1,47 @@
+package com.scala.helloworld
+
+object Implicits {
+  import converter._
+
+  def main(args: Array[String]): Unit = {
+    val integer = Integer(1)
+    println(integer + 10)
+
+    val list = CustomList(List(5,4,3,2,1))
+    val sorted1 = list.sort1(i => i)
+    val sorted2 = list.sort2(i => i)
+    println(sorted1)
+    println(sorted2)
+
+    println(10 print (_ + 5))
+  }
+
+  implicit class IntPrint(x: Int) {
+    def print(f: Int => Int): Int = {
+      println(s"$x")
+      f(x)
+    }
+  }
+
+}
+
+case class Integer(i: Int)
+
+object converter {
+  implicit def integerToInt(integer: Integer): Int = {
+    integer.i
+  }
+}
+
+case class CustomList[T](list: List[T]) {
+  def sort1[B](f: T => B)(implicit order: Ordering[B]): List[T] = {
+    list.sortBy(f)(order)
+  }
+
+  // the same as previous
+  def sort2[B : Ordering](f: T => B): List[T] = {
+    list.sortBy(f)(implicitly[Ordering[B]])
+  }
+}
+
+
